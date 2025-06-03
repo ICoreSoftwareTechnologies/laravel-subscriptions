a6c48ac4ced0bfc6555ad8a18b1fe52364a5a356
Jithin Lukose <jithintom08@users.noreply.github.com>
2025-06-03 18:05:26 +0530

    boot patch

--- src/Providers/SubscriptionsServiceProvider.php
+++ src/Providers/SubscriptionsServiceProvider.php
@@ -58,8 +58,12 @@ class SubscriptionsServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Publish Resources
-        $this->publishesConfig('rinvex/laravel-subscriptions');
-        $this->publishesMigrations('rinvex/laravel-subscriptions');
-        ! $this->autoloadMigrations('rinvex/laravel-subscriptions') || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->publishes([
+            __DIR__ . '/../../config/config.php' => config_path('rinvex.subscriptions.php'),
+        ], 'config');
+        $this->publishes([
+            __DIR__ . '/../../database/migrations/' => database_path('migrations'),
+        ], 'migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
 }
